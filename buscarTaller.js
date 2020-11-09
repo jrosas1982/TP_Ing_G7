8de05020486b17c544ee7c1e167f48434751511c
Juan Pablo <jrosas1982@gmail.com>
2020-11-09 19:59:03 -0300

    Imagen en el popUp de talleres

--- buscarTaller.js
+++ buscarTaller.js
@@ -1,5 +1,5 @@
-var taller1 = { nombre: "Picasso", latitud: -34.667104, longitud: -58.370278, nroDeTelefono: "02320-683182", franjaHoraria: "De 07:00 a 18:30" }
-var taller2 = { nombre: "Rembrandt", latitud: -34.600104, longitud: -58.380678, nroDeTelefono: "02320-683182", franjaHoraria: "De 08:00 a 20:00" }
+var taller1 = { nombre: "Picasso", latitud: -34.667104, longitud: -58.370278, nroDeTelefono: "02320-683182", franjaHoraria: "De 07:00 a 18:30", img: "img/img_avatar.png" };
+var taller2 = { nombre: "Rembrandt", latitud: -34.600104, longitud: -58.380678, nroDeTelefono: "02320-683182", franjaHoraria: "De 08:00 a 20:00", img: "img/img_avatar.png" };
 let listaTalleres = [taller1, taller2];
 var map;
 
@@ -7,13 +7,10 @@ function cargarAlIniciar() {
     console.log("llega a cargar");
     var ungsLocation = [-34.5221554, -58.7000067];
     map = L.map('mapid').setView(ungsLocation, 15);
-
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
-
 }
-
 
 function mostrarEnMapa() {
     var nombreDelTaller = $("#inputNombreTaller").val(); //Obtengo el valor ingresado del Label NombreTaller
@@ -24,7 +21,7 @@ function mostrarEnMapa() {
             coordenada.push(listaTalleres[i].latitud);
             coordenada.push(listaTalleres[i].longitud);
             var tallerMarker = L.marker(coordenada);
-            tallerMarker.bindPopup("Nombre del Taller: " + listaTalleres[i].nombre + "<br/>" + "Franja Horaria: " + listaTalleres[i].franjaHoraria + "<br/>" + "Numero de Telefono : " + listaTalleres[i].nroDeTelefono);
+            tallerMarker.bindPopup("Nombre del Taller: " + listaTalleres[i].nombre + "<br/>" + "Franja Horaria: " + listaTalleres[i].franjaHoraria + "<br/>" + "Numero de Telefono : " + listaTalleres[i].nroDeTelefono + "<img style='width:100%' src='" + listaTalleres[i].img + "' alt=''>");
             map.setView(coordenada, 15);
             L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
                 attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
