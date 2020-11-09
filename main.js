3664ccfe4ec1f7e547d15fde0796dafc51fab5b4
Juan Pablo <jrosas1982@gmail.com>
2020-11-09 08:49:11 -0300

    Inscripción taller y mapas

--- main.js
+++ main.js
@@ -1,9 +1,59 @@
-console.log("llega hasta acá");
+var map;
 
-$("#buscaTaller").click(function() {
+function bootstrapo() {
+    var ungsLocation = [-34.5221554, -58.7000067];
+    map = L.map('mapid').setView(ungsLocation, 15);
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+}
 
-    console.log("llegó a buscar taller");
+function getMapa() {
+    return map;
+}
 
-    $("#carouselExampleInterval").hide();
-    document.getElementById("contenidoTaller").innerHTML = '<h2> soy un taller </h2>';
-});
+function dibujarMarcadores(coordenada) {
+    var primeraUbicacion = [coordenada[0], coordenada[1]];
+    map.setView(primeraUbicacion, 15);
+    marker = new L.marker([(coordenada[0]), coordenada[1]]).addTo(map);
+}
+
+function loguearEmail() {
+    var email = $("#exampleInputEmail1").val();
+    //  localStorage.setItem("email", email);
+    sessionStorage.setItem("email", email);
+    console.log("email guardado  " + sessionStorage.getItem("email").toLocaleUpperCase());
+    $("#usuario").innerHTML = "<p> Hola </p>";
+    $("#login").show();
+
+    // console.log("email guardado  " + localStorage.getItem("email").toLocaleUpperCase());
+}
+
+function limpiarSelector() {
+    $("#direccionNormalizada").empty().append("<option>Seleccione la dirección</option>");
+}
+
+function normalizar() {
+    var url = "http://servicios.usig.buenosaires.gob.ar/normalizar/?direccion=";
+    var calle = $("#inputDirección").val();
+    var localizacion = '&geocodificar=TRUE';
+    var xhttp = new XMLHttpRequest();
+    var coordenada = [];
+    var mapa1 = getMapa();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var lista = JSON.parse(this.responseText);
+            for (var index = 0; index < (Object.keys(lista.direccionesNormalizadas).length); index++) {
+                console.log("Acá las localidades " + lista.direccionesNormalizadas[index].nombre_localidad);
+                document.getElementById("direccionNormalizada").innerHTML += ' <option value=' + index + '>' + lista.direccionesNormalizadas[index].direccion + ', ' + lista.direccionesNormalizadas[index].nombre_localidad + '</option>';
+                coordenada = [lista.direccionesNormalizadas[index].coordenadas.y, lista.direccionesNormalizadas[index].coordenadas.x];
+                dibujarMarcadores(coordenada);
+            }
+            console.log("lista: " + Object.keys(lista.direccionesNormalizadas));
+            // console.log("response: " + this.responseText);
+            // console.log(this.status);
+        }
+    };
+    xhttp.open("GET", url + calle + localizacion, true);
+    xhttp.send();
+}
---
+++ view.js
@@ -0,0 +1,37 @@
+// var map;
+
+// function bootstrapo() {
+
+//     var ungsLocation = [-34.5221554, -58.7000067];
+//     map = L.map('mapid').setView(ungsLocation, 15);
+
+//     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+//         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+//     }).addTo(map);
+// }
+
+// function getMapa() {
+//     return map;
+// }
+
+// L.polygon([
+//     L.latLng(-34.515594, -58.705654),
+//     L.latLng(-34.523503, -58.714062),
+//     L.latLng(-34.519177, -58.719890),
+//     L.latLng(-34.511089, -58.711374),
+//     L.latLng(-34.514062, -58.707909),
+//     L.latLng(-34.513824, -58.707584)
+// ]).addTo(map);
+
+// var ungsMarker = L.marker(ungsLocation);
+// ungsMarker.addTo(map);
+
+// var cluster = L.markerClusterGroup();
+// cluster.addLayers([
+//     ungsMarker,
+//     L.marker([-34.533755, -58.692713]),
+//     L.marker([-34.516181, -58.716625])
+// ]);
+
+// cluster.addTo(map);
+//map.addLayer(cluster);
